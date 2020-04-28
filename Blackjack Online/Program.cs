7f953b4b257025e0378124b16f93b1bf7c6df54d
MattiasOdo <Mattias.odo@utb.helsingborg.se>
2020-04-28 13:58:19 +0200

    update blackjack

--- Blackjack Online/Program.cs
+++ Blackjack Online/Program.cs
@@ -13,15 +13,16 @@ namespace Blackjack_Online
             int sum = 0;
             Carddeck carddeck = new Carddeck();
             carddeck.ShuffleCards();
-            Console.WriteLine("Välkommen till scam city. Vill du spela? (ja/nej)");
+            Console.WriteLine("Welcome to scam city! Do you want to play? (yes/no)");
             //string choice = Console.ReadLine();
-            string choice = "ja";
-            if (choice == "ja")
+            string choice = "yes";
+            if (choice == "yes")
             {
                 carddeck.ShuffleCards();
                 Card c1 = carddeck.GetFirstCard();
                 Card c2 = carddeck.GetFirstCard();
-                
+                Card c3 = carddeck.GetFirstCard();
+
 
 
                 c1.Printcard();
@@ -29,14 +30,32 @@ namespace Blackjack_Online
                 c2.Printcard();
                 c2.PrintValue();
                 sum = c1.PrintValue() + c2.PrintValue();
-                Console.WriteLine(sum);
+                Console.WriteLine("You have "+ sum +" points!");
 
-                Console.WriteLine("Hora vill du ha hit?");
-                string hit = "ja";
-                while(hit == "ja")
+                Console.WriteLine("Do you want to hit a child?");
+                string hit = "yes";
+                while(hit == "yes")
                 {
-                    
+                    c3.Printcard();
+                    c3.PrintValue();
+                    sum = c1.PrintValue() + c2.PrintValue() + c3.PrintValue();
+                    Console.WriteLine("You have " + sum + " points!");
 
+                    if (sum == 21)
+                    {
+                        Console.WriteLine("Grattis Sagge du vann");
+                    }
+                    if (sum < 21)
+                    {
+                        Console.WriteLine("You have less then 21 points. Do you want another card?");
+                        hit = Console.ReadLine();
+
+
+                    }
+                    if (sum > 21)
+                    {
+
+                    }
                 }
 
 
